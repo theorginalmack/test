23721564974f3697a2096b2dc374c0714a7a6544
Mackiee <57823944+theorginalmack@users.noreply.github.com>
2026-03-27 22:59:25 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 provider "oci" {
   fingerprint  = var.fingerprint
-  private_key  = base64decode(var.private_key)
+  private_key  = var.private_key
   region       = var.region
   tenancy_ocid = var.tenancy_ocid
   user_ocid    = var.user_ocid
